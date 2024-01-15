a97da93fd4445c7de78799f216d8a7f6db73f91d
Leo Chung <leo.chung97@gmail.com>
2024-01-15 15:10:58 -0500

    Initial commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Leo Chung on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
