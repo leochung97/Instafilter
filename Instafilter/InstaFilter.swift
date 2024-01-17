07061582478480e1a57ac0bf7300ee97451109a1
Leo Chung <leo.chung97@gmail.com>
2024-01-17 16:16:53 -0500

    Start new view for project

---
+++ Instafilter/InstaFilter.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct InstaFilter: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
+    func changeFilter() {
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+
+                Spacer()
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+
+                    Spacer()
+
+                    // share the picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
+    }
+}
+
+#Preview {
+    InstaFilter()
+}
